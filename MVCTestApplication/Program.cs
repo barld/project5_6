23407f01e118f1fe3cb7420315fc0f042a94fd6a
barld <bakeds@gmail.com>
2016-10-25 10:23:21 +0200

    move test controllers to seperate project

--- MVCTestApplication/Program.cs
+++ MVCTestApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -6,7 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVC.DevelopmentTest
+namespace MVCTestApplication
 {
     class Program
     {

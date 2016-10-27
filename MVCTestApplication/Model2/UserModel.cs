161d3c91106823144fcd658bc3be7e7a6909bf56
JFConstancia <JFConstancia@hotmail.com>
2016-10-27 16:35:11 +0200

    -

---
+++ MVCTestApplication/Model2/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using MVC.Controller;
+using DataModels;
+using ConsoleApplication1;
+using System;
+using System.Web;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVC.View;
+using MVCTestApplication;
+using MVC.DevelopmentTest.Controller;
+
+namespace MVCTestApplication.Model
+{
+    class UserModel
+    {
+        public void userRegister(UserController controller, User userdata)
+        {
+            controller.dbInit();
+            controller.db.collectionInsertUser(userdata);
+        }
+    }
+}

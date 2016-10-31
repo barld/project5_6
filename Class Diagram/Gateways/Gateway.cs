f8328a1e9809097af12b2999532dca91627a4cce
BrutalCoding <dhbreedeveld@gmail.com>
2016-10-31 23:12:59 +0100

    Added SelectAll method to the main gateway. Removed the interface and class of DatabaseConnection since we now make use of Gateways. Deleted old class diagram, renamed revisited diagram to class diagram.

--- Class Diagram/Gateways/Gateway.cs
+++ Class Diagram/Gateways/Gateway.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataModels
+namespace DataModels.Gateways
 {
     public abstract class Gateway<Model>
     {
@@ -42,6 +42,22 @@ namespace DataModels
         public async Task InsertMany(IEnumerable<Model> collection)
         {
             await Collection.InsertManyAsync(collection);
+        }
+
+        public async Task<IEnumerable<Model>> SelectAll(Model model)
+        {
+            //This will retrieve all data from a collection back into a list, without filtering the data first.
+            try
+            {
+                Console.WriteLine($"***All results from '{collectionName}' have been retrieved***");
+                return await Collection.Find(new BsonDocument()).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***Failed to add {collectionName}..***");
+                Console.WriteLine(ex.Message);
+                return new List<Model>();
+            }
         }
     }
 }

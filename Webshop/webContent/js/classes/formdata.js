575d0f223729f04cc31cdfe9c7873f474e544311
Andy Bhadai <abhadai@gmail.com>
2016-10-31 14:57:36 +0100

    Fixed null-checking on elements, still need to refactor this into a class

--- Webshop/webContent/js/classes/formdata.js
+++ Webshop/webContent/js/classes/formdata.js
@@ -28,12 +28,13 @@ class FormHandler{
 	    // The RequestHeader can be any, by the server accepted, file
 	    xhr.setRequestHeader('Content-type', options.requestHeader, true);
 
-	    xhr.send(options.data !== null ? options.data : null);
+	    xhr.send(options.data !== null ? JSON.stringify(options.data) : null);
 
 		// Function to fire off when the server has send a response
 	    xhr.onload = function () {
+	        console.log("TEST!");
 	        options.ajaxFunction();
-	        //console.log(xhr.response);
+	        console.log(xhr.response);
 		}
 	}
 }
--- Webshop/webContent/js/functions.js
+++ Webshop/webContent/js/functions.js
@@ -1,8 +1,6 @@
 ﻿function Login() {
     var userInformation = { username: document.getElementById("login_email").value, password: document.getElementById("login_password").value };
     var formHandler = new FormHandler();
-    console.log(userInformation.username);
-    console.log(userInformation.password);
 
     formHandler.addValue('username', userInformation.username.value);
     formHandler.addValue('password', userInformation.password.value);
@@ -10,7 +8,7 @@
     formHandler.startCall({
         requestHeader: 'application/json',
         method: 'POST',
-        url: 'http://localhost:8080/index.html',
+        url: 'http://localhost:8080/api/user/login',
         data: formHandler,
         ajaxFunction: function () {
             console.log(userInformation.username, userInformation.password);
@@ -20,8 +18,12 @@
 
 // Display the username in the username textbox (saved with localStorage)
 function displayUsername() {
-    if (localStorage.getItem("login_email")) {
-        document.getElementById("login_email").value = localStorage.getItem("login_email");
-        document.getElementById("login_password").focus();
+    if (localStorage.getItem("login_email") !== null) {
+
+        // Check if the element exists (need to wrap this up within a class to make it cleaner) and set the text of the textbox
+        if (typeof (document.getElementById('login_email')) != 'undefined' && document.getElementById('login_email') != null) {
+            document.getElementById("login_email").value = localStorage.getItem("login_email");
+            document.getElementById("login_password").focus();
+        }
     }
 }

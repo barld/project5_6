832d50746c2e6157203a4329e649550d98b074b2
Andy Bhadai <abhadai@gmail.com>
2016-11-03 14:09:30 +0100

    Reading out menuItems is possible

--- Webshop/webContent/web/src/PlatformMenu.vue
+++ Webshop/webContent/web/src/PlatformMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="submenu">
         <li v-for="menuItem in menuItems">
-            <a v-bind:href="'/#platform/' + menuItem._id">{{ menuItem.PlatformTitle }}</a>
+            <a href="#">{{ menuItem.PlatformTitle }}</a>
         </li>
     </ul>
 </template>
@@ -18,18 +18,21 @@
                 var base = this;
 
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "/api/platforms");
+                xhr.open("GET", "/api/platform/all");
 
                 // The RequestHeader can be any, by the server accepted, file
                 xhr.setRequestHeader('Content-type', "Application/JSON", true);
 
                 // Function to fire off when the server has send a response
                 xhr.onload = function () {
-                    base.menuItems = JSON.parse(xhr.response).MenuItems;
+                    base.menuItems = JSON.parse(xhr.response);
                 };
 
                 xhr.send();
             }
+        },
+        created: function(){
+            this.getData();
         }
     }
 </script>

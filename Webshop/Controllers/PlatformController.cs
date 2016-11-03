cf81fc4552d8e15ae5e92850c2d17decca38baa3
barld <bakeds@gmail.com>
2016-11-03 12:33:59 +0100

    add endpoint for platforms

---
+++ Webshop/Controllers/PlatformController.cs
@@ -0,0 +1,26 @@
+﻿using DataModels;
+using MVC.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Controllers
+{
+    public class PlatformController : MVC.Controller.Controller
+    {
+        private Context context;
+
+        public override void AfterConstruct()
+        {
+            base.AfterConstruct();
+            this.context = new Context();
+        }
+
+        public ViewObject GetAll()
+        {
+            return Json(context.Platforms.GetAll().Result);
+        }
+    }
+}

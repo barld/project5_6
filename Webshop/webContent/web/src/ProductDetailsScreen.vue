1d3196632992ca768fe85eeebf01c511a92bd153
Andy Bhadai <abhadai@gmail.com>
2016-11-07 12:01:14 +0100

    Added product details

---
+++ Webshop/webContent/web/src/ProductDetailsScreen.vue
@@ -0,0 +1,13 @@
+<popup v-on:close="close" id="login_screen" class="login_screen">
+    {{ product }}
+</popup>
+
+<script>
+    export default{
+        data: function(){
+            return{
+                product:[]
+            }
+        }
+    }
+</script>

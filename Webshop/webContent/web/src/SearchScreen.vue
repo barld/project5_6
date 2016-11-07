ac8e5ddf642128b548cbccceff2e4232914a110c
Andy Bhadai <abhadai@gmail.com>
2016-11-07 09:14:26 +0100

    Fixed: maybe need to implement a watcher to get a full list of games

--- Webshop/webContent/web/src/SearchScreen.vue
+++ Webshop/webContent/web/src/SearchScreen.vue
@@ -2,6 +2,7 @@
     <div>
         <input v-model="searchValue" list="GameSearchList" type="search" id="game_search" name="game_search" class="u-full-width" placeholder="Zoeken" @keyup="searchGame"/>
         <datalist id="GameSearchList" v-for="game in searchResult">
+            <option :value="game.GameTitle"></option>
         </datalist>
     </div>
 </template>
@@ -17,27 +18,17 @@
         methods:{
             searchGame:function(){
                 var base = this;
-
-                if(this.searchValue.length > 3){
-                    var xhr = new XMLHttpRequest();
-                    xhr.open("POST", "/api/product/search");
-
-                    // The RequestHeader can be any, by the server accepted, file
-                    xhr.setRequestHeader('Content-type', "Application/JSON", true);
-                    // Function to fire off when the server has send a response
-                    xhr.onload = function () {
-
-                        for(var i = 0; i < result.length; i ++){
-                            var gameTitle = result[i].GameTitle;
-                            document.getElementById("search_suggestions").innerHTML = "<option value=' " + result[i].EAN + "'>" + result[i].GameTitle + "</option>";
-                        }
-
-                        document.getElementById("search_suggestions").focus();
-                    };
-
-                    xhr.send(JSON.stringify({value: this.searchValue}));
-                }
-            }
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "/api/product/search");
+                // The RequestHeader can be any, by the server accepted, file
+                xhr.setRequestHeader('Content-type', "Application/JSON", true);
+                // Function to fire off when the server has send a response
+                xhr.onload = function () {
+                    base.searchResult = JSON.parse(xhr.response);
+                    console.log(base.searchResult);
+                };
+                xhr.send(JSON.stringify({value: this.searchValue}));
+            }s
         }
     }
 </script>

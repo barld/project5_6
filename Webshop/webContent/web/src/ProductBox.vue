083125b0f2fbb76ef1b34bb3563083a815bbadbf
Polyanos <gregorhoogstad@gmail.com>
2016-11-10 11:56:42 +0100

    Added a tittle attribute to the titel span, witch displays the full title no matter what.

--- Webshop/webContent/web/src/ProductBox.vue
+++ Webshop/webContent/web/src/ProductBox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="three columns product">
         <div style="margin: 0 auto;">
-            <span class="productbox_gametitle">{{product.GameTitle}}</span>
+            <span class="productbox_gametitle"v-bind:title="product.GameTitle">{{product.GameTitle}}</span>
             <span class="productbox_platformtitle">({{product.Platform.PlatformTitle}})</span>
             <div class="thumbnail_container">
                 <img v-bind:src="product.Image[0]" alt="" class="thumbnail">

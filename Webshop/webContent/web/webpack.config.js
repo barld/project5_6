ae7fca1a6193240837fd7191aa0dd4b44127e0aa
Andy Bhadai <abhadai@gmail.com>
2017-01-16 10:18:16 +0100

    Added admin directory, including files and added multiple entry points for client- and adminside

--- Webshop/webContent/web/webpack.config.js
+++ Webshop/webContent/web/webpack.config.js
@@ -2,11 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    main: './src/main.js',
+    admin: './src/admin/admin.js'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].build.js'
   },
   module: {
     rules: [

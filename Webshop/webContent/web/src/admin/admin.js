4712a48ce0847eef73ebd210ba1d8d94666893ba
Andy Bhadai <abhadai@gmail.com>
2017-01-16 14:25:51 +0100

    Implementing adding games with Promise AJAX-calls

--- Webshop/webContent/web/src/admin/admin.js
+++ Webshop/webContent/web/src/admin/admin.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-
-new Vue({
-    el:'#app',
-    data:{
-        message: 'Hi, Admin!'
-    }
-});
+// import Vue from 'vue'
+//
+// new Vue({
+//     el:'#app',
+//     data:{
+//         message: 'Hi, Admin!'
+//     }
+// });

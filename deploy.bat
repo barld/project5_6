5775cfdedb4480f550b5a975511e458ef7f1eadf
barld <bakeds@gmail.com>
2017-01-20 16:23:26 +0100

    update deploy again

--- deploy.bat
+++ deploy.bat
@@ -4,12 +4,12 @@ taskkill /F /IM Webshop.exe
 SET dest=%~dp0productionEnv
 
 if not exist "%dest%" mkdir "%dest%"
-xcopy /Y /s "%~dp0bin\Debug" "%dest%"
+xcopy /Y /s "%~dp0bin\Webshop\Debug" "%dest%"
 
 SET webDest="%dest%/webContent"
 
 if not exist %webDest% mkdir %webDest%
-xcopy /Y /s "%~dp0webContent\web" %webDest%
+xcopy /Y /s "%~dp0Webshop\webContent\web" %webDest%
 
 copy /Y "%~dp0startApp.bat" "%dest%/startApp.bat"
 START "" "%dest%/startApp.bat"

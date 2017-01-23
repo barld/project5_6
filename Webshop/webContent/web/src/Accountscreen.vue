4fc179a78cb4f0947e2b868ed3642a9380208436
Polyanos <gregorhoogstad@gmail.com>
2017-01-23 12:24:49 +0100

    Removed the extra button and will use instead the existing button "bekijk producten". Removed code that has been made obselete by the change.

--- Webshop/webContent/web/src/Accountscreen.vue
+++ Webshop/webContent/web/src/Accountscreen.vue
@@ -9,7 +9,6 @@
             </div>
         </div>
         <div class="container">
-			<button @click="close_account_screen">Sluit accountscherm</button>
         	<div class="row">
         		<div class="six columns">
                     <div class="element-container">
@@ -62,9 +61,6 @@
             },
             product:function(){
                 this.$emit("product");
-            },
-            close_account_screen:function(){
-            	this.$emit("close_account_screen");
             }
         }
     }

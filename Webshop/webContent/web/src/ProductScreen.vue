07e480b400f0e7c5744e9d89936a4412d3fe477a
BrutalCoding <dhbreedeveld@gmail.com>
2017-02-01 14:05:22 +0100

    PolyAnos fixed bug with displaying wish/fav buttons

--- Webshop/webContent/web/src/ProductScreen.vue
+++ Webshop/webContent/web/src/ProductScreen.vue
@@ -10,7 +10,7 @@
 
 <script>
     export default{
-        props:['user_status'],
+        props:['user_status','event_bus'],
         data: function(){
             return{
                 products:[],
@@ -21,6 +21,7 @@
         },
         methods: {
             getData: function () {
+                console.log("called getdata");
                 var base = this;
 
                 var xhr = new XMLHttpRequest();
@@ -37,6 +38,7 @@
                     products.forEach(product => base.already_own_game(product, "Wish List"));
                     products.forEach(product => base.already_own_game(product, "Favourite List"));
                     base.products = products;
+                    console.log("Retrieved game data");
                 };
 
                 xhr.send();
@@ -59,7 +61,7 @@
                 xhr.setRequestHeader('Content-type', "Application/JSON", false);
                 var gameInformation = {EAN:product.EAN, TitleOfList:listTitle};
                 xhr.send(JSON.stringify(gameInformation));
-                
+                console.log("TEST");
             //    return true;
                 // Function to fire off when the server has send a response
                 xhr.onload = function () {
@@ -95,6 +97,9 @@
 
         },
         created: function(){
+            console.log(this.event_bus);
+            this.event_bus.$on('user_logged_in', function(){ this.getData();}.bind(this));
+            this.event_bus.$on('user_logged_out', function(){ this.getData();}.bind(this));
             this.getData();
         },
         close: function(){

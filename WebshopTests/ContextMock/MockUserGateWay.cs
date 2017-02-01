94865e45c90eb0a28a157c73939f31d25cf9c0cd
Polyanos <gregorhoogstad@gmail.com>
2017-02-01 19:35:55 +0100

    Fixed a small error in the usergateway, apparently there was a duplicate method in there.

--- WebshopTests/ContextMock/MockUserGateWay.cs
+++ WebshopTests/ContextMock/MockUserGateWay.cs
@@ -96,10 +96,5 @@ namespace WebshopTests.ContextMock
         {
             throw new NotImplementedException();
         }
-
-        public IEnumerable<GameWishListStatisticsJsonDataModel> GetGameWishListStatistics(int amount, ICollection<Genre> genre)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
